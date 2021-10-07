b07dea98f4ebe6812aa5dfcfe1011f70f7a7d790
flexsidee <flexsidee@gmail.com>
2021-10-07 01:01:44 +0100

    latest update

--- cusotm-half-progressbar/index.js
+++ cusotm-half-progressbar/index.js
@@ -1,6 +1,6 @@
 let css = document.getElementById('max');
 css.oninput =  () =>{
-    let y= css.value - 135 ;
+    let y= css.value - 75 ;
     let z = y/1.8;
     document.querySelector('span').style.transform = `rotate(${css.value}deg)`;
     document.querySelector('small').innerText= z.toFixed(2) + '%';
